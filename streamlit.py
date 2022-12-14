7e825fb9b6514f465c3aeec2e5ede71dd0bbca99
shinbrooke <91584566+shinbrooke@users.noreply.github.com>
2022-12-15 02:37:12 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -189,11 +189,33 @@ for i in range(len(academic_list)): #학문
       globals()[academic_list[i]+'_minus_list'].append(int(change[2:-1]))
 
 #예시
-st.write('앵그리버드-추가 글자수 리스트', angrybird_plus_list)
-st.write('앵그리버드-삭제 글자수 리스트', angrybird_minus_list)
+#st.write('앵그리버드-추가 글자수 리스트', angrybird_plus_list)
+#st.write('앵그리버드-삭제 글자수 리스트', angrybird_minus_list)
+c_plist = [angrybird_plus_list, crashlandingonyou_plus_list, gameserver_plus_list, itzy_plus_list, maplephantom_plus_list, myname_plus_list, readymadelife_plus_list, skycastle_plus_list, ssglanders_plus_list, transformer_plus_list]
+c_mlist = [angrybird_minus_list, crashlandingonyou_minus_list, gameserver_minus_list, itzy_minus_list, maplephantom_minus_list, myname_minus_list, readymadelife_minus_list, skycastle_minus_list, ssglanders_minus_list, transformer_minus_list]
+group_labels = ['추가', '삭제']
+colors = ['red', 'blue']
+selected_item = st.radio("대중문화/서브컬처", culture_list)	
 
+for i in range(len(culture_list)):
+    if selected_item == culture_list[i]:
+        fig_c1 = ff.create_distplot([c_plist[i], c_mlist[i]], group_labels, colors=colors, opacity = 0.6)
+        fig_c1.update_layout(title_text='추가/삭제 글자수 양상')
+        st.plotly_chart(fig_c1, use_container_width=True)
 
+a_plist = [aesthetic_plus_list, call_plus_list, epidemic_plus_list, greekromanmyth_plus_list, hungarianrevolution_plus_list, imjin_plus_list, montyhall_plus_list, officiallanguage_plus_list, pascaltriangle_plus_list, spotlight_plus_list]
+a_mlist = [aesthetic_minus_list, call_minus_list, epidemic_minus_list, greekromanmyth_minus_list, hungarianrevolution_minus_list, imjin_minus_list, montyhall_minus_list, officiallanguage_minus_list, pascaltriangle_minus_list, spotlight_minus_list]
+group_labels = ['추가', '삭제']
+colors = ['red', 'blue']
+selected_item = st.radio("학문", academic_list)	
 
+for i in range(len(academic_list)):
+    if selected_item == academic_list[i]:
+        fig_a1 = ff.create_distplot([a_plist[i], a_mlist[i]], group_labels, colors=colors, opacity = 0.6)
+        fig_a1.update_layout(title_text='추가/삭제 글자수 양상')
+        st.plotly_chart(fig_a1, use_container_width=True)
+        
+        
 st.markdown("### 3.2. 시간에 따른 수정 양상 변화")
 
 # [필요하면 코드 사용하기] 키워드(페이지)별 편집 기간
